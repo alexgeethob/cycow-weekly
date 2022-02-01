4261490932fa518abdc59aca857d15cec867b0f8
Alex Zhang <alexzhang05@gmail.com>
2022-02-01 09:39:49 -0600

    fixed challenge api

--- pages/api/challenges/[id].js
+++ pages/api/challenges/[id].js
@@ -2,7 +2,6 @@ import { getInfo } from "/firebase/retrieve";
 
 export default async function handler(req, res) {
   const {id} = req.query;
-  console.log(id);
   const info = await getInfo(id);
   if(info.error !== null){
     res.status(400);
@@ -10,6 +9,5 @@ export default async function handler(req, res) {
     return;
   }
   delete info.error;
-  console.log(info.data);
   res.status(200).send(info.data);
 }
