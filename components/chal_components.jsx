5ed89342a6f3789d1dcab5d53d65226c3858da42
Alex Zhang <alexzhang05@gmail.com>
2022-02-11 21:28:43 -0600

    added better looking flag submission

--- components/chal_components.jsx
+++ components/chal_components.jsx
@@ -1,16 +1,22 @@
-//For actual challenges
 import React from "react";
 import ReactCanvasConfetti from "react-canvas-confetti";
-import styles from "/styles/Chal.module.css";
+import styles from "/styles/FlagForm.module.css";
 import { getFlag } from "/firebase/retrieve";
 
+
+function Incorrect(props) {
+  return <div className={styles.incorrect}>Try Again.</div>;
+}
+function Correct(props) {
+  return <div className={styles.correct}>Correct!</div>;
+}
 
 export class FlagForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "", isSubmitted: false };
+    this.state = { value: "", isSubmitted: false, result: 0, };
     this.id = props.id;
-    this.correct = "";
+    this.flag = "";
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -22,24 +28,48 @@ export class FlagForm extends React.Component {
   async handleSubmit(event) {
     this.setState({ isSubmitted: true });
     event.preventDefault();
-    if (this.correct === "") {
+    if (this.flag === "") {
       const data = await getFlag(this.id);
       if (data.error) {
         alert("Something went wrong, please try again");
         return;
       }
-      this.correct = data.flag;
+      this.flag = data.flag;
     }
 
-    if (this.state.value.toLowerCase().trim() === this.correct) {
-      alert("Correct!");
+    if (this.state.value.toLowerCase().trim() === this.flag) {
+      this.setState({result : 1});
+      this.fireConfetti();
+      setTimeout(() => {
+        this.setState({result : 0});
+      }, 4000);
     } else {
-      alert("Try Again.");
+      this.setState({result : -1});
+      setTimeout(() => {
+        this.setState({result : 0});
+      }, 4000);
     }
     this.setState({ isSubmitted: false });
   }
+
+  fireConfetti(){
+    return
+  }
+
   //{this.state.isSubmitted && <ReactCanvasConfetti className={styles.confetti}></ReactCanvasConfetti>}
   render() {
+    
+    var resultBox;
+    if(this.state.result == 1){
+      resultBox = (<Correct/>);
+    }
+    else if(this.state.result == -1){
+      resultBox = (<Incorrect/>);
+    }
+    else{
+      resultBox = null;
+    }
+
     return (
       <div>
         <form onSubmit={this.handleSubmit} className={styles.form}>
@@ -56,6 +86,7 @@ export class FlagForm extends React.Component {
             Submit
           </button>
         </form>
+        {resultBox}
       </div>
     );
   }
